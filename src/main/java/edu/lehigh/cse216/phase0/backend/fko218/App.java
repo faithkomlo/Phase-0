2adeb9cb47b13e88b79d6b94d51d4dbdda40cc6e
Faith Komlo <faithkomlo@dyn158121.cc.lehigh.edu>
2017-01-30 16:54:08 -0500

    Initial checkin of a basic maven project

---
+++ src/main/java/edu/lehigh/cse216/phase0/backend/fko218/App.java
@@ -0,0 +1,13 @@
+package edu.lehigh.cse216.phase0.backend.fko218;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
